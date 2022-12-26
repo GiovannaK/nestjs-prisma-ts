619a9f009eebf97efd9a5a235e37e2738bc0685e
Giovanna Cunha <giovannakelli@gmail.com>
2022-12-26 20:11:10 -0300

    feat: refactor

--- src/application/usecases/send-notification.spec.ts
+++ src/application/usecases/send-notification.spec.ts
@@ -1,23 +1,17 @@
-import { Notification } from '../entities/Notification';
+import { InMemorynotificationRepository } from '../../../test/respositories/inMemory-notifications-repositories';
 import { SendNotification } from './send-notification';
-
-const notifications: Notification[] = [];
-
-const notificationRepository = {
-  async create(notification: Notification) {
-    notifications.push(notification);
-  },
-};
 
 describe('Send notification', () => {
   it('should be able to create a notification', async () => {
-    const sendNotification = new SendNotification(notificationRepository);
-    await sendNotification.execute({
+    const notificationsRepository = new InMemorynotificationRepository();
+    const sendNotification = new SendNotification(notificationsRepository);
+    const { notification } = await sendNotification.execute({
       category: 'email',
       content: 'Hello world',
       recipientId: 'some-id',
     });
 
-    expect(notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
   });
 });
