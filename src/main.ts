6b16741ad3663eaee354ae18f3337d3694508ce1
Giovanna Cunha <giovannakelli@gmail.com>
2023-01-01 22:35:15 -0300

    feat: kafka connection

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { KafkaConsumerService } from '@infra/messaging/kafka/kafka-consumer.service';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -5,6 +6,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const kafkaConsumerService = app.get(KafkaConsumerService);
+
+  app.connectMicroservice({
+    strategy: kafkaConsumerService,
+  });
   await app.listen(3000);
 }
 bootstrap();
